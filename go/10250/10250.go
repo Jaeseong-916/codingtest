2316ec4c0e962c804b2360a87b5725bb16c427c6
JaeSeong <rornfl916@gmail.com>
2025-11-16 00:59:07 +0900

    251115 Commit Not done 10250.

--- go/10250/10250.go
+++ go/10250/10250.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -11,14 +12,43 @@ func main() {
 	var t, h, w, n int
 
 	fmt.Fscanln(reader, &t)
-	var res = make([]int, t)
+	var res []string
 
-	for i := range t {
+	for range t {
 		fmt.Fscanln(reader, &h, &w, &n)
-
+		res = append(res, room_num(h, w, n))
 	}
 }
 
-// 처음부터 h만큼 1씩 더하면서 진행.
-// h 이후 w-h만큼 유지하면서 진행.
-// h + w - h 이후 h만큼 1씩 감소하면서 진행.
+func room_num(h int, w int, n int) string {
+	if n == 1 {
+		return "101"
+	}
+	var num []int
+	var cnt int = 1
+	for i := 0; i < h; i++ { // 처음부터 h까지 1씩 더하면서 진행.
+		cnt = cnt + i
+		num = append(num, cnt)
+	}
+	for i := h; i < h-w; i++ { // h 부터 w-h까지 h씩 더하면서 진행.
+		cnt = cnt + h
+		num = append(num, cnt)
+	}
+	for i := h - 1; i < 0; i-- { // w 이후 h만큼 1씩 감소하면서 진행.
+		cnt = cnt + i
+		num = append(num, cnt)
+	}
+	for i := range num {
+		if num[i] <= n {
+			if num[i]%n == 0 {
+				if i < 10 {
+					return "10" + strconv.Itoa(i)
+				} else {
+					return "1" + strconv.Itoa(i)
+				}
+			} else {
+
+			}
+		}
+	}
+}
